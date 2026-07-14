0a1b5ae34b8a8c6eef2b09f03f10ff980e3d8715
sourav-me <souravpaul.mailme@gmail.com>
2026-07-14 23:42:20 +0600

    first glimps

---
+++ practiceTask.js
@@ -0,0 +1,48 @@
+let currentAge = 30;
+currentAge = 29;
+console.log(currentAge);
+
+const height = 5.5;
+console.log(height);
+
+const name = 'Sourav';
+
+console.log(name);
+console.log(typeof name);
+
+console.log(typeof height);
+
+const firstNumber = 20;
+const secondNumber = 30;
+
+const sumOfTwoNumber = firstNumber + secondNumber;
+
+console.log(sumOfTwoNumber);
+console.log (typeof sumOfTwoNumber)
+
+const multiplyOfTwoNumber = firstNumber * secondNumber;
+console.log(multiplyOfTwoNumber);
+
+const divideOfTwoNumber = firstNumber / secondNumber;
+console.log(divideOfTwoNumber);
+const numberWithTwoDigit = divideOfTwoNumber.toFixed(2);
+console.log(numberWithTwoDigit);
+
+const modulo = firstNumber % secondNumber;
+console.log(modulo);
+
+const subOfTwoNumber = firstNumber - secondNumber;
+console.log(subOfTwoNumber);
+
+let newNumber = 5;
+
+console.log(newNumber);
+
+newNumber++;
+console.log(newNumber);
+
+newNumber--;
+console.log(newNumber);
+
+newNumber += newNumber;
+console.log(newNumber);
