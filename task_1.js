fa6a48864f8a0ca861b93e81c8fe2e9e2be88fec
sourav-me <souravpaul.mailme@gmail.com>
2026-07-15 00:01:50 +0600

    task 1 and task 2

---
+++ task_1.js
@@ -0,0 +1,15 @@
+const hello = 'Hello JavaScript!'
+console.log(hello);
+
+const introName = 'My name is Alex.';
+console.log(introName);
+
+const aboutMe = 'I am excited to become a Web Developer.'
+console.log(aboutMe);
+
+console.log('\n****Another way Check Output****\n');
+
+// another way
+
+const taskOutput = 'Hello JavaScript!\nMy name is Alex.\nI am excited to become a Web Developer.'
+console.log(taskOutput);
---
+++ task_2.js
@@ -0,0 +1,12 @@
+let num1 = 25;
+let num2 = 10;
+
+let divisionAns = num1/num2;
+
+let forTwoDigit = divisionAns.toFixed(1);
+
+console.log('Addition: ',num1+num2);
+console.log('Subtraction: ',num1-num2);
+console.log('Multiplication: ',num1*num2);
+console.log('Division: ',forTwoDigit);
+console.log('Modulus: ',num1%num2);
